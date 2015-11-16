b34337c71964795ec56da501c6380eb166ad28df
Jonas <mr-jstraub@users.noreply.github.com>
2015-11-16 06:38:47 +0100

    - main content ".container" has been moved to sub-templates, so that the flexibility is higher - new box and column layout for the nodes and components (.box & .cbox) - stylesheet/design has been updated - added cluster building functionality (initial commit)

--- js/app.js
+++ js/app.js
@@ -7,7 +7,13 @@
  * Author: Jonas Straub 
  */
 
-var app = angular.module('nodeviewApp', ['ngRoute']);
+var app = angular.module('nodeviewApp', ['ngRoute', 'dndLists', 'xeditable']);
+
+app.run(function(editableOptions, editableThemes) {
+  editableThemes.bs3.inputClass = 'input-sm';
+  editableThemes.bs3.buttonsClass = 'btn-xs';
+  editableOptions.theme = 'bs3';
+});
 
 app.config(function($routeProvider) {
     $routeProvider
@@ -18,6 +24,10 @@ app.config(function($routeProvider) {
     .when('/legend', {
         templateUrl: 'legend.html',
         controller: 'NodeViewController as nodeCtrl'
+    })
+    .when('/build', {
+        templateUrl: 'cluster_build.html',
+        controller: 'BuildController as buildCtrl'
     })
     .when('/settings/services', {
         templateUrl: 'settings_services.html',
@@ -44,6 +54,167 @@ app.config(function($routeProvider) {
       redirectTo:'/'
     });
 });
+
+
+/**
+ * Cluster-Build Controller
+ **/
+app.controller('BuildController', ['$scope', 'DefEnvironment', function($scope, DefEnvironment){
+    /* {int} maxNodes Maximum number of nodes allowed */
+    $scope.maxNodes = 1000;
+    /* {String} defHostname Default hostname of node */
+    $scope.defHostname = 'node.example.com';
+    /* {Service} curService Currently selected service */
+    $scope.curService = null;
+    /* {Service[]} List of services */
+    $scope.services = DefEnvironment.services;
+    /* {Component[]} List of components */
+    $scope.comps = DefEnvironment.comps;
+    /* {Component[]} Components of selected service w/o circ. structure */
+    $scope.buildcomps = [];
+    /* {Node[]} Nodes of cluster */
+    $scope.buildnodes = [{'name': 'master.example.com', 'comps': [], 'cardinality': 1}, {'name': 'mgmt.example.com', 'comps': [], 'cardinality': 1}, {'name': 'worker##{2}.example.com', 'comps': [], 'cardinality': 3}];
+    /* {Object} Different configurations */
+    $scope.config = {'cols': 'col-md-3'};
+
+    /**
+     * Generates hostnames according to the given name.
+     * Allowed formats:
+     *   {0} => First occurence will be used for count until cardinality
+     *          is reached. In this case 0 represents the beginning, this 
+     *          can be any given positive number.
+     *   #{0} => The # in front of the start number adds trailing zeros 
+     *           to the beginning of the hostname, e.g. 01,02,03 instead
+     *           of 1,2,3
+     * @returns Array of calculated hostnames
+     */
+    $scope.genHostnames = function(name, cardinality){
+        if(typeof(name) !==  'string'){
+            console.warn('Cannot process this data type (non-string)');
+            return [];
+        }
+
+        // get hostname info info
+        var isTrZeros = (name.match(/(#)+\{\d+\}/)) ? true : false;
+        // remove trailing info from name
+        name = name.replace(/#+/, '');
+        // determine av. start of host_partial (e.g. 1000, 0, 22,...)
+        var startMatch = name.match(/\{(\d+)\}/);
+        var start = 0;
+        // generated hostnames
+        var hosts = [];
+        var pfx = name;
+        var sfx = '';
+
+        // nothing to do, hostname is fine
+        if(!startMatch && cardinality <= 1){
+            return [name];
+        }
+
+        // host_partial start was defined
+        if(startMatch){
+            start = Number((name.match(/\{(\d+)\}/) || [0, 0])[1]);
+            startSplit = name.split(/\{\d+\}/);
+            pfx = startSplit[0];
+            sfx = startSplit[1];
+        }
+
+        // determine host_partial size
+        var size = ((start + cardinality) + "").length;
+        
+        // generate hostnames
+        for(var i = 0; i < cardinality; i++){
+            var hostPartial = (start++) + "";
+            if(isTrZeros){
+                hostPartial = "000" + hostPartial;
+                hostPartial = hostPartial.substr(hostPartial.length-size);
+            }
+            hosts.push(pfx + hostPartial + sfx);
+        }
+
+        return hosts;
+    };
+
+    /**
+     * New service selected, prepare components.
+     */
+    $scope.selectComps = function(selService){
+        $scope.buildcomps = [];
+        $scope.curService = selService;
+        for(var key in selService.comps){
+            var comp = selService.comps[key];
+            $scope.buildcomps.push({'name':comp['name'], 'id': comp['id'], 'baseColor': selService.baseColor, 'fontColor': selService.fontColor});
+        }
+    };
+
+    /**
+     * Make sure the entered cardinality + the number of existing nodes
+     * does not exceed 1000 nodes.
+     * @param {int} newNum entered cardinality
+     * @param {int} oldNum old cardinality
+     */
+    $scope.checkCardinality = function(newNum, oldNum){
+        if((newNum + $scope.numClusterNodes() - oldNum) > $scope.maxNodes){
+            return "Only 1000 nodes are allowed."
+        }
+    };
+
+    /**
+     * Counts the current amount of nodes in this cluster
+     * @returns amount of nodes
+     */
+    $scope.numClusterNodes = function(){
+        var result = 0;
+        for(var key in $scope.buildnodes){
+            result += 1 * $scope.buildnodes[key]['cardinality'];
+        }
+        console.debug('Num cluster nodes ' + result);
+        return result;
+    };
+
+    /**
+     * Adds a new node to this cluster.
+     */
+    $scope.addNode = function(){
+        if($scope.numClusterNodes() >= $scope.maxNodes){
+            console.warn('Cannot add more than ' + $scope.maxNodes + ' nodes');
+            return;
+        }
+        $scope.buildnodes.push({'name': $scope.defHostname, 'comps': [], 'cardinality': 1});
+    };
+
+    /**
+     * Removes the selected node from the cluster
+     * @param {int} idx index of node
+     */
+    $scope.removeNode = function(idx){
+        console.debug('Removing node with idx: ' + idx);
+        $scope.buildnodes.splice(idx, 1);
+    };
+
+    /**
+     * Checks whether the selected node already has a component
+     * with this id.
+     * @param event drag event
+     * @param item selected component
+     * @param node selected node
+     * @returns false in case node is already available, otherwise the comp
+     */
+    $scope.isUnique = function(event, item, node){
+        if('node' in item && item['node'] == node.name){
+            return item;
+        }
+        for(var k in node.comps){
+            if(item.id == node.comps[k]['id']){
+                return false;
+            }
+        }
+        item['node'] = node.name;
+        return item;
+    };
+
+}]);
+
 
 
 /**
